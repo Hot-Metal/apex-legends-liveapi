ff6643405eb251f7926c0a91f5b6ed7cc43bf12d
Aaron Cooper <mr.aaroncooper@gmail.com>
2025-03-31 22:54:17 -0400

    initialize ts and cs files

--- scripts/build-csharp.sh
+++ scripts/build-csharp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-protoc -I=. --csharp_out=../csharp/HotMetal.ApexLegendsLiveApi -I=../ ../events.proto --csharp_opt=base_namespace=HotMetal.ApexLegendsLiveApi
+protoc -I=. --csharp_out=../csharp/HotMetal.ApexLegendsLiveApi -I=../ ../events.proto
