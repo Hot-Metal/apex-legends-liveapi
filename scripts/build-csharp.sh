927c8d4e8ae9bfc0be22bd49748a6a589fd56015
Aaron Cooper <mr.aaroncooper@gmail.com>
2025-03-31 22:46:55 -0400

    shell script updates and package updates

--- scripts/build-csharp.sh
+++ scripts/build-csharp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-protoc -I=. --csharp_out=../csharp/HotMetal.ApexLegendsLiveApi events.proto
+protoc -I=. --csharp_out=../csharp/HotMetal.ApexLegendsLiveApi -I=../ ../events.proto --csharp_opt=base_namespace=HotMetal.ApexLegendsLiveApi
--- scripts/build-ts.sh
+++ scripts/build-ts.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# Leverages the protobuf ts library for file generation
 cd ../ts/apex-legends-liveapi
 npm run start
